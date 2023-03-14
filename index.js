52d0ab3ace4b210ce3e75592f997dc3da304c80e
Bradigson <71954800+Bradigson@users.noreply.github.com>
2023-03-13 21:53:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-const whitelist = ['*'];
+
 
 app.use(express.json());
 
-app.use(cors({
-    origin:whitelist
-}));
+app.use(cors('*'));
 
 app.use('/api/v1', routers);
 
